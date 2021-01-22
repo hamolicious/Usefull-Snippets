efa212ba7f5ba3d6b5b02f9d46b560a94bf43b48
Hamolicious <56944714+hamolicious@users.noreply.github.com>
2021-01-22 01:16:35 +0000

    added credits

--- translate.py
+++ translate.py
@@ -3,6 +3,8 @@ Takes in a value and maps it from a range to another range, for example:
 
 6 came from a range between 1, 500 but you want it to be 0, 100
 new_val = translate(6, 1, 500, 0, 100)
+
+CREDITS: https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another Adam Luchjenbroers
 """
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):
